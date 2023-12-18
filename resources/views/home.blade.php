2fd5175ab6818100a793b81a83ff92fba35806de
Hycarus <mirkomarasco08@gmail.com>
2023-12-19 01:45:07 +0900

    changed home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,20 @@
 @section('title', 'Home')
 
 @section('content')
-<main>
-    <h1>Home Page</h1>
-</main>
+    @include('partials.jumbo')
+    <main>
+        <div class="container d-flex">
+            <div class="row my-5">
+                @foreach (config('comics.key') as $comic)
+                    <div class="my-card col-12 col-md-4 col-lg-2">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                        <h3>{{ $comic['series'] }}</h3>
+                    </div>
+                @endforeach
+            </div>
+            <button>LOAD MORE</button>
+        </div>
+    </main>
+    @include('partials.service')
 
 @endsection
