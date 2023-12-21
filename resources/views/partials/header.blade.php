c1eb3188b702f6021276ece1d718730b73f860d3
Hycarus <mirkomarasco08@gmail.com>
2023-12-21 09:28:16 +0900

    added selected page and link on header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,8 +6,12 @@
             </div>
             <div class="my-div">
                 <ul class="d-flex justify-content-between">
-                    <li>CHARACTERS</li>
-                    <li>COMICS</li>
+                    <li class="{{ Route::currentRouteName() == 'home' ? 'selected-page' : '' }}">
+                        <a href="{{ route('home') }}">HOME</a>
+                    </li>
+                    <li class="{{ Route::currentRouteName() == 'comics' ? 'selected-page' : '' }}">
+                        <a href="{{ route('comics') }}">COMICS</a>
+                    </li>
                     <li>MOVIES</li>
                     <li>TV</li>
                     <li>GAMES</li>
